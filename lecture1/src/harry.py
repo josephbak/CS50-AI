ee9485302c3716813e62b088dcef9b3e5de27137
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-24 10:13:07 -0500

    comments added to harry.py file

--- lecture1/src/harry.py
+++ lecture1/src/harry.py
@@ -1,14 +1,14 @@
 from logic import *
 
-rain = Symbol("rain")
-hagrid = Symbol("hagrid")
-dumbledore = Symbol("dumbledore")
+rain = Symbol("rain") # It is raining.
+hagrid = Symbol("hagrid") # Harry visited Hagrid.
+dumbledore = Symbol("dumbledore") # Harry visited Dumblerdore.
 
 knowledge = And(
-    Implication(Not(rain), hagrid),
-    Or(hagrid, dumbledore),
-    Not(And(hagrid, dumbledore)),
-    dumbledore
+    Implication(Not(rain), hagrid), # If it wasn't raining, Harry visited Hagrid.
+    Or(hagrid, dumbledore), # Harry visited one of Hagrid and Dumbledore.
+    Not(And(hagrid, dumbledore)), # Harry visited one of Hagrid and Dumbledore.
+    dumbledore # Harry visited Dumbledore.
 )
 
 print(model_check(knowledge, rain))
